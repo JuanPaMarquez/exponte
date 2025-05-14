36acac703add36dd4bc9a12c0e846631ee00c15d
Juan Pablo Marquez Sanchez <juan013sanchez@gmail.com>
2025-05-13 19:55:13 -0500

    refactor: Eliminar constante WIDTH y reemplazarla por valores directos en el componente de inicio de sesión

--- src/ui/Login/Login.tsx
+++ src/ui/Login/Login.tsx
@@ -5,7 +5,6 @@ import InicioSesion from "@/ui/Login/InicioSesion";
 import Register from "@/ui/Login/Register";
 import { ButtonLogin } from "@/components/Buttons";
 
-const WIDTH = "560px"
 
 export default function Login() {
 
@@ -14,17 +13,17 @@ export default function Login() {
 
   
   return (
-    <div id="loginComponent" className={`max-w-200 h-120 max-[${WIDTH}]:max-w-80 w-full flex bg-[var(--color-foreground)] text-[var(--color-background)] rounded-lg overflow-hidden shadow-lg transition-all duration-300`}>
+    <div id="loginComponent" className={`max-w-200 h-120 max-[560px]:max-w-80 w-full flex bg-[var(--color-foreground)] text-[var(--color-background)] rounded-lg overflow-hidden shadow-lg transition-all duration-300`}>
       <div 
-        className={`flex-1 z-8 max-[${WIDTH}]:hidden transition-transform duration-300 
-        ${register ? `min-[${WIDTH}]:translate-x-full` : `min-[${WIDTH}]:translate-x-0`}`}
+        className={`flex-1 z-8 max-[560px]:hidden transition-transform duration-300 
+        ${register ? `min-[560px]:translate-x-full` : `min-[560px]:translate-x-0`}`}
       >
         <img className="w-full h-full object-cover" src={`/${register ? "imgRegister" : "imgLogin"}.webp`} alt="image login" />
       </div>
       <div 
         id="loginForm" 
         className={`flex-1 flex flex-col items-center justify-center gap-2 my-4 transition-transform duration-300 
-        ${register ? `min-[${WIDTH}]:-translate-x-full` : `min-[${WIDTH}]:translate-x-0`}`}
+        ${register ? `min-[560px]:-translate-x-full` : `min-[560px]:translate-x-0`}`}
       >
         {register ? <Register /> : <InicioSesion />}
         <ButtonLogin 
