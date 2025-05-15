7e130a2a17a6920b6ca5ede96a318d98fdbeffd1
Juan Pablo Marquez Sanchez <juan013sanchez@gmail.com>
2025-05-14 21:30:13 -0500

    feat: Agregar componente LayoutDashboard con navegación y estilos

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 export default function Dashboard() {
   return (
-    <div className="bg-[var(--color-foreground)] text-[var(--color-background)] p-2">
-      <h1 className="text-2xl font-bold">Dashboard</h1>
+    <div>
+      <p>hola mundo</p>
     </div>
   )
 }
